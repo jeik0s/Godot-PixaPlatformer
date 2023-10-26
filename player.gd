39db1f65324cde3723523310be7ae656e82dcece
Jakub Zaremba <jzaremba@iterative.pl>
2023-10-26 20:25:47 +0200

    [DAY-3] Learn about simple animation in movement

--- player.gd
+++ player.gd
@@ -9,7 +9,18 @@ extends CharacterBody2D
 @export var ADDITIONAL_FALL_GRAVITY : int = 4
 @export var BUNNY_HOP : bool = true
 
+@onready var animatedSprite = $AnimatedSprite2D
+
+# enum ANIMATION_STATE {IDLE, RUN}
+
 var charVelocity = Vector2.ZERO
+
+func _ready():
+	animatedSprite.play("Idle")
+	
+	# CHECK ME!
+	# check how to change sprite
+	# animatedSprite.name("res://PlayerGreenSkin.tres")
 
 func _physics_process(_delta):
 	apply_gravity()
@@ -17,9 +28,16 @@ func _physics_process(_delta):
 	input.x = Input.get_action_strength("right") - Input.get_action_strength("left");
 	
 	if input.x == 0:
+		animatedSprite.play("Idle")
 		apply_friction();
 	else:
+		animatedSprite.play("Run")
 		apply_acceleration(input.x);
+		
+		if input.x > 0:
+			animatedSprite.set_flip_h(true)
+		elif input.x < 0:
+			animatedSprite.set_flip_h(false)
 	
 	if is_on_floor():
 		if BUNNY_HOP:
@@ -30,13 +48,19 @@ func _physics_process(_delta):
 				velocity.y = JUMP_FORCE
 				
 	else:
+		animatedSprite.play("Jump")
 		if Input.is_action_just_released("up") and velocity.y < JUMP_RELEASE_FORCE: 
 			velocity.y = JUMP_RELEASE_FORCE
 		
 		if velocity.y > 0:
 			velocity.y += ADDITIONAL_FALL_GRAVITY
-	
+			
+	var was_in_air = not is_on_floor()
 	move_and_slide()
+	
+	if is_on_floor() and was_in_air:
+		animatedSprite.play("Run")
+		animatedSprite.set_frame(1)
 	
 func apply_gravity():
 		velocity.y += GRAVITY
